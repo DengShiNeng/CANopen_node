008f7cfd4863a4231f3b227ea5ce9ea4c05fba4d
DengShiNeng <1957821927@qq.com>
2025-07-17 11:48:48 +0800

    logs

---
+++ src/master_package/master_package/master_node.py
@@ -0,0 +1,45 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from send_can_msgs.msg import SendMotorCommands
+from recive_can_msgs.msg import ReciveMotorCommands
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('master_node')
+        self.publisher_ = self.create_publisher(SendMotorCommands, '/can/in/motor_commands', 10)
+        self.subscrip_ = self.create_subscription(ReciveMotorCommands, '/can/out/motor_commands', self.subscrip_callback, 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = SendMotorCommands()
+        msg.node = 1
+        msg.str = "Hello"
+        msg.vaule = 100
+        self.publisher_.publish(msg)
+       
+
+    def subscrip_callback(self, msg):
+        self.get_logger().info('Subscrip: "%d, %s, %d"' % (msg.node, msg.str, msg.vaule))
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
